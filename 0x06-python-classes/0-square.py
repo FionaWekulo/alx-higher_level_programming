1b16df50af76981624cbcba36290d425ebeff002
FionaWekulo <fionawekulo+1@gmail.com>
2022-10-09 15:50:58 -0700

    pythn classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue October 10 01:48:54 2022
+@author: Fiona Wekulo
+"""
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue October 10 01:21:54 2022
+@author: Fiona Wekulo
+"""
+class Square:
+    """Class Square that has attributes. Instantiation with size
+    Attributes:
+        size (int): The size of the square
+    """
+    def __init__(self, size):
+        """The __init__ method for Square class
+        Args:
+            size: (:obj: 'int'): A private instance size
+        """
+        self.__size = size
