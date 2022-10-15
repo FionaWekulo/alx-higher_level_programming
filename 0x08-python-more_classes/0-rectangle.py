7ba0c171f052b17d730a506b19e211325682263c
FionaWekulo <fionawekulo+1@gmail.com>
2022-10-15 16:05:52 -0700

    More python classes files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+
+Created on Mon 17 2022
+
+@author: Fiona Wekulo
+
+"""
+
+
+class Rectangle:
+    
+    """Empty class Rectangle that defines a rectangle figure
+
+
+    Attributes:
+
+        empty
+
+    """
+  
+
+    
+    pass
